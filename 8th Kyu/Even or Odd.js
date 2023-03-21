e58416d93c2e750b3a74a54618ffba703bbadf9a
LDER <ludjy.dev@gmail.com>
2023-03-20 22:59:43 -0400

    codewar 3/20 push

---
+++ 8th Kyu/Even or Odd.js
@@ -0,0 +1,8 @@
+function evenOrOdd(number) {
+  if( number % 2 === 0){
+    return 'Even'
+  }else 
+    return 'Odd'
+}
+
+//a function that taks a number as a argument adn returns even for even odd for odd
