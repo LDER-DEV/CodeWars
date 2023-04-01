85a497eec3bbe4d4f617ddf6d548e47eef116e09
LDER <ludjy.dev@gmail.com>
2023-03-31 23:28:56 -0400

    3/31 codewar

---
+++ 8th Kyu/Sum of differences in array.js
@@ -0,0 +1,19 @@
+function sumOfDifferences(arr) {
+  if(arr.length <= 1){
+   return 0 
+  }  
+  let final = (Math.max(...arr ) - Math.min(...arr))
+
+return final
+}
+
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
