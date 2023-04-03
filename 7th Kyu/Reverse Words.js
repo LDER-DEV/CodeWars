6e64916facd0f002502ce72e2faad0fe2b4aded7
LDER <ludjy.dev@gmail.com>
2023-04-03 18:38:16 -0400

    code war 4/3

---
+++ 7th Kyu/Reverse Words.js
@@ -0,0 +1,14 @@
+function reverseWords(str) {
+  let final = str.split('').reverse('').join('').split(' ').reverse('').join(' ')
+  
+  return final
+ }
+
+
+
+//  DESCRIPTION:
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
