124b36fcdb5b75b0af7f396d933b48ee31700c4c
LDER <ludjy.dev@gmail.com>
2023-04-08 17:10:54 -0400

    codewar 4/8

---
+++ 8th Kyu/Contamination #1-string.js
@@ -0,0 +1,23 @@
+function contamination(text, char){
+  return text.split('').map(x=> x = char).join('')
+  }
+
+//   8 kyu
+// Contamination #1 -String-
+// JavaScript:
+
+// DESCRIPTION:
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+// STRINGSFUNDAMENTALS
