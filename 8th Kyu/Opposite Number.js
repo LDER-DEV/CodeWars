cc259c3df0e7689ea0c6598706a6dbeca812df7d
LDER <ludjy.dev@gmail.com>
2023-04-14 23:22:42 -0400

    4/14

---
+++ 8th Kyu/Opposite Number.js
@@ -0,0 +1,16 @@
+
+// Opposite Number
+
+function opposite(number) {
+  return number / -1
+}
+
+
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
