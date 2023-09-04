19f18c403a5df9eb5dff65dcf9625f854fff39b4
LDER <ludjy.dev@gmail.com>
2023-09-04 15:18:55 -0400

    codewar 9/4

---
+++ 6th Kyu/Counting Duplicates.js
@@ -0,0 +1,28 @@
+function duplicateCount(text){
+  let textArray = text.toLowerCase().split('').sort()
+  let duplicateArr = []
+ 
+   if (text.length === 0){
+     return 0
+   }
+   for(let i = 0; i < textArray.length; i++){
+   if(textArray[i] === textArray[i+1]){
+     duplicateArr.push(textArray[i])
+   }
+   }
+   console.log(duplicateArr)
+   return [...new Set(duplicateArr)].length
+ }
+
+ 
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
